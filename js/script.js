2ec0998089c3408317947b3fde90e747c5b6e206
João Pedro Biguelini <biguelinijoao@gmail.com>
2020-11-25 12:53:45 -0300

    adicionando mais algumas funções

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
+var showingSouceCode=false;
+var isInEditMode = true;
 function enableEditMode(){
     richTextField.document.designMode = "on";
 }
 
 function execCmd(command){
     richTextField.document.execCommand(command, false, null);
+}
+function execCommandWithArg(command, arg){
+    richTextField.document.execCommand(command, false, arg);
+}
+
+function toggleSouerce(){
+    if(showingSouceCode){
+        richTextField.document.getElementByTagName('body')[0].innerHTML = richTextField.document.getElementByTagName('body')[0].textContent;
+        showingSouceCode=false;
+    } else{
+        richTextField.document.getElementByTagName('body')[0].textContent = richTextField.document.getElementByTagName('body')[0].innerHTML;
+        showingSouceCode=true;
+    }
+}
+
+function toggleEdit(){
+    if(isInEditMode){
+        isInEditMode=false;
+        richTextField.document.designMode = "off";
+    } else{
+        isInEditMode=true;
+        richTextField.document.designMode = "on";
+    }
 }

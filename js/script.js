3c472161823483e138b773fe046d348e96e5898b
João Pedro Biguelini <biguelinijoao@gmail.com>
2021-01-30 11:32:25 -0300

    finish

--- js/script.js
+++ js/script.js
@@ -1,36 +1,10 @@
-var showingSouceCode=false;
-var isInEditMode = true;
+var pressedCtrl = false;
 function enableEditMode(){
     richTextField.document.designMode = "on";
 }
-
 function execCmd(command){
     richTextField.document.execCommand(command, false, null);
 }
-function execCommandWithArg(command, arg){
-    richTextField.document.execCommand(command, false, arg);
-}
-
-function toggleSouerce(){
-    if(showingSouceCode){
-        richTextField.document.getElementByTagName('body')[0].innerHTML = richTextField.document.getElementByTagName('body')[0].textContent;
-        showingSouceCode=false;
-    } else{
-        richTextField.document.getElementByTagName('body')[0].textContent = richTextField.document.getElementByTagName('body')[0].innerHTML;
-        showingSouceCode=true;
-    }
-}
-
-function toggleEdit(){
-    if(isInEditMode){
-        isInEditMode=false;
-        richTextField.document.designMode = "off";
-    } else{
-        isInEditMode=true;
-        richTextField.document.designMode = "on";
-    }
-}
-
 function createLink(){
     var url = prompt('Enter a URL:', 'http://');
     var selection = richTextField.document.getSelection();
